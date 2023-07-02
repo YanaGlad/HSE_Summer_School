58d5d1ddc64ce4db4ca76bd9fe2cb112559895e7
y.gladkikh <y.gladkikh@tinkoff.ru>
2023-07-02 11:14:51 +0300

    Arrays

--- core/src/com/mygdx/game/javabasics/JavaBasics2.java
+++ core/src/com/mygdx/game/javabasics/JavaBasics2.java
@@ -5,4 +5,18 @@ package com.mygdx.game.javabasics;
  */
 public class JavaBasics2 {
 
+    public static void main(String[] args) {
+
+        int[] array1 = {1, 2, 3, 10, 0};
+
+        int[] array = new int[5];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        array[3] = 10;
+        array[4] = 0;
+
+        String[] strings = new String[100];
+        String[] strings1 = {"aa", "bb", "???"};
+    }
 }
