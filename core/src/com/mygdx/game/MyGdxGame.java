088291de3ad784c43dc96bbef450eefa6a67a68e
y.gladkikh <y.gladkikh@tinkoff.ru>
2023-07-05 06:23:52 +0300

    Lecture 3 - game beginning

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -16,23 +16,64 @@ import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.TimeUtils;
 
 public class MyGdxGame extends Game {
-	// ширина и высота экрана
-	public static final float SCR_WIDTH = 1280;
-	public static final float SCR_HEIGHT = 720;
+    // ширина и высота экрана
+    public static float SCR_WIDTH;
+    public static float SCR_HEIGHT;
+    Texture imgMosquito;
+    float x, y;
 
-	// системные объекты
-	SpriteBatch batch; // ссылка на объект, отвечающий за вывод изображений
-	OrthographicCamera camera; // пересчитывает размеры для различных экранов
+    float vx, vy; // скорости движения по х и y
+    float width, height;
+    Texture imgBackGround; // фоновое изображение
 
-	@Override
-	public void create () {
-		batch = new SpriteBatch(); // создать объект, отвечающий за вывод изображений
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, SCR_WIDTH, SCR_HEIGHT);
-	}
+    // системные объекты
+    SpriteBatch batch; // Объект, отвечающий за вывод изображений
+    OrthographicCamera camera; // пересчитывает размеры для различных экранов
 
-	@Override
-	public void dispose () {
-		batch.dispose();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch(); // создать объект, отвечающий за вывод изображений
+        camera = new OrthographicCamera();
+
+        SCR_WIDTH = Gdx.graphics.getWidth();
+        SCR_HEIGHT = Gdx.graphics.getHeight();
+
+        camera.setToOrtho(false, SCR_WIDTH, SCR_HEIGHT);
+
+        imgBackGround = new Texture("swamp0.jpg");
+        imgMosquito = new Texture("mosq0.png");
+
+        vx = 5;
+        vy = 5;
+
+        x = 150;
+        y = 150;
+
+        width = SCR_WIDTH / 4;
+        height = SCR_HEIGHT / 4 + 100;
+    }
+
+    @Override
+    public void render() {
+        batch.begin();
+
+        batch.draw(imgBackGround, 0, 0, SCR_WIDTH, SCR_HEIGHT);
+
+        batch.draw(imgMosquito, x, y, width, height);
+
+        if (x < 0 || x > SCR_WIDTH - width) vx = -vx;
+        if (y < 0 || y > SCR_HEIGHT - height) vy = -vy;
+
+        x += vx;
+        y += vy;
+
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        imgMosquito.dispose();
+        imgBackGround.dispose();
+    }
 }
--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -2,14 +2,20 @@ package com.mygdx.game;
 
 import static com.mygdx.game.MyGdxGame.*;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
+
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
+
+		SCR_WIDTH = 1280;
+		SCR_HEIGHT = 720;
+
 		config.setWindowedMode((int) SCR_WIDTH, (int) SCR_HEIGHT);
 		config.setTitle("Game");
 		new Lwjgl3Application(new MyGdxGame(), config);
